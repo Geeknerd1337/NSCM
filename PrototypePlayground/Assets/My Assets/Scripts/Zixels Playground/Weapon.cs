84c469f80df7c06e90733877c65e0e6ffddf8f28
Geeknerd1337 <geeknerd1337@gmail.com>
2020-08-18 17:24:28 -0500

    Some gun Stuff

---
+++ PrototypePlayground/Assets/My Assets/Scripts/Zixels Playground/Weapon.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour
+{
+
+    #region Private Members
+    private Animator weaponAnimator;
+    #endregion
+
+    [Header("Weapon Object")]
+    [SerializeField] private Weapon_SO weaponObject;
+
+
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Get the weapon's Animator
+        weaponAnimator = GetComponent<Animator>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        HandleGunInput();
+    }
+
+    void HandleGunInput()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            FireGun();
+        }
+
+        if (Input.GetButtonDown("Fire2"))
+        {
+            //Toggle the aim animation
+            weaponAnimator.SetBool("isAiming", !weaponAnimator.GetBool("isAiming"));
+        }
+
+        if (Input.GetButtonDown("Reload"))
+        {
+            weaponAnimator.Play(weaponObject.reloadAnimation);
+        }
+    }
+
+    void FireGun()
+    {
+        if (!weaponAnimator.GetCurrentAnimatorStateInfo(0).IsName(weaponObject.fireAnimation) && !weaponAnimator.GetCurrentAnimatorStateInfo(0).IsName(weaponObject.fireAnimation))
+        {
+            //Play the fire animation if we press the fire button
+            if (!weaponAnimator.GetBool("isAiming"))
+            {
+                weaponAnimator.Play(weaponObject.fireAnimation);
+            }
+            else
+            {
+                weaponAnimator.Play(weaponObject.aimFireAnimation);
+            }
+        }
+    }
+
+}
---
+++ PrototypePlayground/Assets/My Assets/Scripts/Zixels Playground/Weapon_SO.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Weapon", menuName = "Create/Weapons")]
+public class Weapon_SO : ScriptableObject
+{
+    [Header("Animations")]
+    public string fireAnimation;
+    public string dryFireAnimation;
+    public string aimFireAnimation;
+    public string aimDryFireAnimation;
+    public string reloadAnimation;
+
+    [Header("Stats")]
+    public string damage;
+    
+}
