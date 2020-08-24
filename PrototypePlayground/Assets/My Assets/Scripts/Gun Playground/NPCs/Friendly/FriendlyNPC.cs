cc3e4c4222e94c947b60b8a084f4b2f897e7c7b3
Geeknerd1337 <geeknerd1337@gmail.com>
2020-08-24 11:59:48 -0500

    New Gun

---
+++ PrototypePlayground/Assets/My Assets/Scripts/Gun Playground/NPCs/Friendly/FriendlyNPC.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class FriendlyNPC : MonoBehaviour
+{
+    #region Private Members
+    private NavMeshAgent agent;
+    private Animator friendlyAnimator;
+    #endregion
+
+    [SerializeField]
+    private Transform target;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Get The Agent;
+        agent = GetComponent<NavMeshAgent>();
+        //Get the Animator
+        friendlyAnimator = GetComponent<Animator>();
+
+        GoToCurrentTarget();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        UpdateAI();
+    }
+    void UpdateAI()
+    {
+
+        //Basically check if the agent is moving
+        if (agent.remainingDistance < agent.stoppingDistance)
+        {
+            friendlyAnimator.SetBool("IsWalking", false);
+        }
+        else
+        {
+            friendlyAnimator.SetBool("IsWalking", true);
+        }
+    }
+
+    void GoToCurrentTarget()
+    {
+        agent.SetDestination(target.position);
+    }
+}
