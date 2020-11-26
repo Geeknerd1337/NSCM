6beebe3e5db62d1fcf193867800373f749a19eb4
Geeknerd1337 <geeknerd1337@gmail.com>
2020-11-25 21:46:57 -0600

    Disc Throwe Work (Stll Broken WIP)

--- PrototypePlayground/Assets/My Assets/Scripts/Gun Playground/Weapons/DiscProjectile.cs
+++ PrototypePlayground/Assets/My Assets/Scripts/Gun Playground/Weapons/DiscProjectile.cs
@@ -8,22 +8,63 @@ public class DiscProjectile : MonoBehaviour
     public float damage;
 
     private bool targetFound;
+    private Transform target;
+    private Vector3 midPoint;
+
+    private float angle;
+
+    [Header("Stuff for a spherecast needed for homing in on enemies")]
+    public float searchRange;
+    public float searchSphereWidth;
+    public LayerMask searchMask;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        angle = Random.Range(0, 360f);
     }
 
     // Update is called once per frame
     void Update()
     {
-        MoveForward();
+        if (!targetFound)
+        {
+            MoveForward();
+            LookForTarget();
+        }
+        else
+        {
+
+        }
     }
 
     void MoveForward()
     {
         transform.position += transform.forward * Time.deltaTime * projectileSpeed;
+    }
+
+
+    void LookForTarget()
+    {
+        RaycastHit hit;
+
+        if (Physics.SphereCast(transform.position, searchSphereWidth, transform.forward, out hit, searchRange, searchMask))
+        {
+            EntityLimb e = hit.collider.GetComponent<EntityLimb>();
+
+            if(e != null)
+            {
+                target = e.transform;
+                targetFound = true;
+
+                Vector3 dir = target.position - transform.position;
+                dir = dir.normalized;
+                midPoint = transform.position + dir * Vector3.Distance(transform.position, target.position);
+                Vector3 up = Vector3.up;
+                
+
+            }
+        }
     }
 
     private Vector3 CalculateQuadraticBezierPoint(float t, Vector3 p0, Vector3 p1, Vector3 P2)
@@ -39,9 +80,12 @@ public class DiscProjectile : MonoBehaviour
     private void OnCollisionEnter(Collision collision)
     {
         GameObject g = collision.gameObject;
-        if(g.tag == "Enemy")
+        EntityLimb e = g.GetComponent<EntityLimb>();
+        if (e != null)
         {
-
+            e.DamageEnemy(damage, transform.forward);
+            transform.SetParent(e.transform);
+            this.enabled = false;
         }
         else
         {
