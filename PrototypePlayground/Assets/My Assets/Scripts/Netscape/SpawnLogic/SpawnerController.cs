0de4f703167c49221533eeeb6863ae0d67e3a788
alex <alex@levelism.com>
2020-12-05 17:37:06 -0800

    clean up level and add spawner controller

---
+++ PrototypePlayground/Assets/My Assets/Scripts/Netscape/SpawnLogic/SpawnerController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnerController : MonoBehaviour
+{
+    public List<SpawnerGroup> spawnGroups;
+
+    void Start()
+    {
+        var playerTest = FindObjectOfType<CyberSpaceFirstPerson>();
+        if (playerTest == null)
+        {
+            Debug.LogError("Unable to get player ref");
+        }
+        else
+        {
+            playerRef = playerTest.gameObject;
+        }
+    }
+
+    void Update()
+    {
+        
+    }
+
+    private GameObject playerRef;
+}
