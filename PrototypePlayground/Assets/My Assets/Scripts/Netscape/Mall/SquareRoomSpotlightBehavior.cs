354b65ed01e843bbe1b4ed458024d30dd21221b0
alex <alex@levelism.com>
2020-12-07 00:56:36 -0800

    more lighting tweaks

--- PrototypePlayground/Assets/My Assets/Scripts/Netscape/Mall/SquareRoomSpotlightBehavior.cs
+++ PrototypePlayground/Assets/My Assets/Scripts/Netscape/Mall/SquareRoomSpotlightBehavior.cs
@@ -7,6 +7,8 @@ public class SquareRoomSpotlightBehavior : MonoBehaviour
     public Light spotLight;
     public float spotLightAngleSmall = 1.0f;
     public float spotLightAngleLarge = 88.0f;
+    public Color tightColor = Color.green;
+    public Color wideColor = Color.red;
 
     void Start()
     {
@@ -18,5 +20,6 @@ public class SquareRoomSpotlightBehavior : MonoBehaviour
         var sin = Mathf.Sin(Time.time);
         spotLight.spotAngle = Mathf.Lerp(spotLightAngleSmall, spotLightAngleLarge, sin);
         spotLight.shadowBias = sin;
+        spotLight.color = Color.Lerp(tightColor, wideColor,sin);
     }
 }
