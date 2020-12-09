3276c0899a3f10408ba52c28cd6b7314d9535904
Geeknerd1337 <geeknerd1337@gmail.com>
2020-12-08 22:07:21 -0600

    TrainEffects and Train Models

---
+++ PrototypePlayground/Assets/My Assets/Scripts/Netscape/Effects/MovingTrainObject.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingTrainObject : MonoBehaviour
+{
+
+    public Vector3 direction;
+    public float range;
+    public float speed;
+    private Vector3 originalPosition;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        originalPosition = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += direction * speed * Time.deltaTime;
+
+        if(Vector3.Distance(transform.position,originalPosition) >= range)
+        {
+            transform.position = originalPosition - direction * range * 0.99f;
+        }
+    }
+}
