a7e8d1d27ea8b2f557192238905af9556f1a0c61
Geeknerd1337 <geeknerd1337@gmail.com>
2020-12-11 15:46:06 -0600

    Fixed Issue With Decoupled UI

---
+++ PrototypePlayground/Assets/My Assets/Scripts/Netscape/UI/UIMaster.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class UIMaster : MonoBehaviour
+{
+    public Transform hackElement;
+    public Image hackRadialElement;
+    public Image hackFillElement;
+
+    public Transform grappleElement;
+}
