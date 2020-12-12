e40f8be238403a227fd0252f2f06f4602606b79e
alex <alex@levelism.com>
2020-12-11 18:11:24 -0800

    make the buildindex list. You can now complete the mall level and move to the next level.

---
+++ PrototypePlayground/Assets/My Assets/Scripts/Netscape/Environmental/LevelTransition.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelTransition : MonoBehaviour
+{
+    public BoxCollider box;
+    public int buildIndex = 1;
+    
+    private GameObject player;
+
+    void Start()
+    {
+        player = FindObjectOfType<CyberSpaceFirstPerson>().gameObject;
+
+    }
+
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject == player)
+        {
+            SceneManager.LoadScene(buildIndex, LoadSceneMode.Single);
+        }
+
+    }
+
+}
