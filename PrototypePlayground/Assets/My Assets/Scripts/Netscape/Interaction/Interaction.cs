74728aedaab3981fd88cffcca57b5e8d65f2c5b1
Geeknerd1337 <geeknerd1337@gmail.com>
2020-12-11 20:49:37 -0600

    ObjectInteraction and Portal Event Start

---
+++ PrototypePlayground/Assets/My Assets/Scripts/Netscape/Interaction/Interaction.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Interaction : MonoBehaviour
+{
+
+    public virtual void interaction()
+    {
+
+    }
+}
---
+++ PrototypePlayground/Assets/My Assets/Scripts/Netscape/Player/GlitchControl.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Kino;
+public class GlitchControl : MonoBehaviour
+{
+
+    public DigitalGlitch[] glitches;
+    [Range(0, 1)]
+    public float amt = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        glitches = GetComponentsInChildren<DigitalGlitch>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        foreach(DigitalGlitch g in glitches)
+        {
+            g.intensity = amt;
+        }
+    }
+}
