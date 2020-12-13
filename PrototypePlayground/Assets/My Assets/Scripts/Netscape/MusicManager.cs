d6d3b68d8806dafd124786b987622d62c56fb840
Geeknerd1337 <geeknerd1337@gmail.com>
2020-12-13 14:24:50 -0600

    Music and Music Manager Work

--- PrototypePlayground/Assets/My Assets/Scripts/Netscape/MusicManager.cs
+++ PrototypePlayground/Assets/My Assets/Scripts/Netscape/MusicManager.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 
 public class MusicManager : MonoBehaviour
 {
+
+    public AudioSource ambientSource;
+    public AudioSource combatSource;
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +19,22 @@ public class MusicManager : MonoBehaviour
     void Update()
     {
         
+    }
+
+
+   public void PlayAmbience()
+   {
+        ambientSource.Play();
+        ambientSource.volume = 0;
+        StartCoroutine("AmbienceFadeIn");
+   }
+
+   IEnumerator AmbienceFadeIn()
+    {
+        while(ambientSource.volume < 1)
+        {
+            ambientSource.volume += Time.deltaTime / 4f;
+            yield return null;
+        }
     }
 }
---
+++ PrototypePlayground/Assets/My Assets/Scripts/Netscape/SoundTrigger.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class SoundTrigger : MonoBehaviour
+{
+
+    public UnityEvent triggerEvent;
+    private bool triggered;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            if (!triggered)
+            {
+                triggerEvent.Invoke();
+                triggered = true;
+            }
+        }
+    }
+}
