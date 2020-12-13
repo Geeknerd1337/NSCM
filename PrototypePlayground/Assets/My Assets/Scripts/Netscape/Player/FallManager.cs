2d396fc4658f7b90398255fa827edbe92cffc0c7
Geeknerd1337 <geeknerd1337@gmail.com>
2020-12-13 15:25:38 -0600

    Fall Volumes and Fall Triggers

---
+++ PrototypePlayground/Assets/My Assets/Scripts/Netscape/Player/FallManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FallManager : MonoBehaviour
+{
+    public ParticleSystem part;
+    public AudioSource a;
+    public Vector3 originalPosition;
+    // Start is called before the first frame update
+    void Start()
+    {
+        originalPosition = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Die()
+    {
+        PlayerStats p = GetComponent<PlayerStats>();
+        if(p != null)
+        {
+            p.DamagePlayer(5f, transform.position);
+        }
+        a.Play();
+        part.Play();
+
+        transform.position = originalPosition;
+        Physics.SyncTransforms();
+    }
+}
