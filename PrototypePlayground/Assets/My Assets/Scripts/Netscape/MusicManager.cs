185bac2a2356e4908440ec9efcced303c66bc4d4
Geeknerd1337 <geeknerd1337@gmail.com>
2020-12-13 15:04:57 -0600

    Level Work, More Sounds, Polish

--- PrototypePlayground/Assets/My Assets/Scripts/Netscape/MusicManager.cs
+++ PrototypePlayground/Assets/My Assets/Scripts/Netscape/MusicManager.cs
@@ -8,11 +8,17 @@ public class MusicManager : MonoBehaviour
     public AudioSource ambientSource;
     public AudioSource combatSource;
 
+    public AudioClip combatStart;
+    public AudioClip combatLoop;
+
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        combatSource.Play();
+        ambientSource.Play();
+        combatSource.volume = 0f;
+        ambientSource.volume = 0f;
     }
 
     // Update is called once per frame
@@ -24,7 +30,6 @@ public class MusicManager : MonoBehaviour
 
    public void PlayAmbience()
    {
-        ambientSource.Play();
         ambientSource.volume = 0;
         StartCoroutine("AmbienceFadeIn");
    }
@@ -36,5 +41,20 @@ public class MusicManager : MonoBehaviour
             ambientSource.volume += Time.deltaTime / 4f;
             yield return null;
         }
+    }
+
+    IEnumerator CombatFade()
+    {
+        while(combatSource.volume < 1)
+        {
+            combatSource.volume += Time.deltaTime / 4f;
+            ambientSource.volume -= Time.deltaTime / 4f;
+            yield return null;
+        }
+    }
+
+    public void StartCombatMusic()
+    {
+        StartCoroutine("CombatFade");
     }
 }
