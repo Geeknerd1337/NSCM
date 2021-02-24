384bc846e7c7cc5b9057033783383dfda32a82aa
Geeknerd1337 <geeknerd1337@gmail.com>
2021-02-23 21:39:34 -0600

    Fixed FOV Kick bug on Sniper Aim, Fixed Sniper Staying Zoomed In On reload

--- PrototypePlayground/Assets/My Assets/Scripts/Netscape/Weapons/Weapon.cs
+++ PrototypePlayground/Assets/My Assets/Scripts/Netscape/Weapons/Weapon.cs
@@ -165,6 +165,7 @@ public class Weapon : MonoBehaviour
             if (shotsLeft < weaponObject.clipSize && playerStats.ammoTypes[ammoType] != 0)
             {
                 weaponAnimator.Play(weaponObject.reloadAnimation);
+                weaponAnimator.SetBool("isAiming", false);
                 PlayWeaponSound(weaponObject.reloadSound);
                 if (playerStats.ammoTypes[ammoType] >= (weaponObject.clipSize - shotsLeft))
                 {
