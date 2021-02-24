38d21b84b597d6211b1c3506f895079df80152cc
Geeknerd1337 <geeknerd1337@gmail.com>
2021-02-23 21:30:51 -0600

    Fade/Flash System Added

---
+++ PrototypePlayground/Assets/My Assets/Scripts/Netscape/World Scripter/TeleportObject.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+/// <summary>
+/// A class for moving one transform to the location of another
+/// </summary>
+public class TeleportObject : MonoBehaviour
+{
+    /// <summary>
+    /// The transform we are going to teleport
+    /// </summary>
+    [SerializeField]
+    private Transform traveller;
+
+    /// <summary>
+    /// The transform we are teleporting the traveller to
+    /// </summary>
+    [SerializeField]
+    private Transform destination;
+
+    /// <summary>
+    /// An event to be invoked when the traveller is teleported
+    /// </summary>
+    [SerializeField]
+    private UnityEvent teleportEvents;
+    
+    /// <summary>
+    /// Teleports the traveller transform to the destination, invoking the teleport events.
+    /// </summary>
+    public void Teleport()
+    {
+        traveller.transform.position = destination.position;
+        teleportEvents.Invoke();
+        //This ensures the player will be teleported
+        Physics.SyncTransforms();
+    }
+}
