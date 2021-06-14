b13982c458f1e89f55a3b1c7ca6f81333d102f4c
Geeknerd1337 <geeknerd1337@gmail.com>
2021-06-13 22:50:29 -0500

    Basic Set Up For Node I/O System

---
+++ PrototypePlayground/Assets/Scripts/Netscape/Logic/Logic Nodes/LogicNode.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LogicNode : MonoBehaviour
+{
+    public bool output;
+}
---
+++ PrototypePlayground/Assets/Scripts/Netscape/Logic/LogicInput.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class LogicInput : MonoBehaviour
+{
+    public enum State
+    {
+        True = 0,
+        False = 1
+    }
+
+    public LogicNode inputNode;
+    public State validValue = State.True;
+    public bool output;
+
+    public void Update()
+    {
+        bool input = inputNode.output;
+        bool check = (int)validValue == 1 ? true : false;
+
+        if (input == check)
+        {
+            output = true;
+        }
+        else
+        {
+            output = false;
+        }
+
+    }
+}
