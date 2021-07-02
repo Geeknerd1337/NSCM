8a524c4847f188e0960dfa38fa19c8bddb334a2b
Jake K <55055481+Jake-NSW@users.noreply.github.com>
2021-07-02 17:58:40 +1000

    Started work on Console GUI + Changed folders around

---
+++ PrototypePlayground/Assets/Netscape Cybermind/Engine/Scripts/Console System/Command System/BaseCommands.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using UnityEngine;
+
+namespace Armadillo.Netscape
+{
+    public static class BaseCommands
+    {
+        [ConsoleCmd("quit", Help = "Quits the game.")]
+        private static void QuitGame()
+        {
+            Application.Quit();
+        }
+
+        [ConsoleCmd("echo", Help = "Echos the inputted string")]
+        private static void EchoMessage(string input)
+        {
+            Debug.Log(input);
+        }
+
+        [ConsoleCmd("clear", Help = "Clears the console log")]
+        private static void ClearConsoleLog()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
