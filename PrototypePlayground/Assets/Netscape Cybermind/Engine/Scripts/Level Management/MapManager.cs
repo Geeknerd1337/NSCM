de589120c8a55aa07e449613867237f9aa1bd05a
Jake K <55055481+Jake-NSW@users.noreply.github.com>
2021-07-03 02:35:42 +1000

    Started work on new map management system

---
+++ PrototypePlayground/Assets/Netscape Cybermind/Engine/Scripts/Level Management/MapManager.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Armadillo.Netscape
+{
+    public class MapManager
+    {
+        public static void LoadMap(Map map)
+        {
+
+        }
+    }
+
+}
