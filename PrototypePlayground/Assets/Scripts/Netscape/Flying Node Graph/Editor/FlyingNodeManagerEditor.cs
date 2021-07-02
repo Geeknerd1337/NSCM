43c44a2da5edb83ed6b2c4d9bf693ec4c3f8f981
Geeknerd1337 <geeknerd1337@gmail.com>
2021-07-02 11:58:50 -0500

    Node Graph Work

---
+++ PrototypePlayground/Assets/Scripts/Netscape/Flying Node Graph/Editor/FlyingNodeManagerEditor.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof (FlyingNodeManager))]
+public class FlyingNodeManagerEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+
+        FlyingNodeManager nodeManager = (FlyingNodeManager)target;
+
+        DrawDefaultInspector();
+
+        if (GUILayout.Button("BAKE NODES"))
+        {
+            nodeManager.BakeNodes();
+        }
+
+        if (GUILayout.Button("TOGGLE GIZMOS"))
+        {
+            nodeManager.ToggleGizmos();
+        }
+
+        if (GUILayout.Button("TOGGLE SEARCH RADIUS WIREFRAME"))
+        {
+            nodeManager.ToggleWireGizmos();
+        }
+    }
+}
